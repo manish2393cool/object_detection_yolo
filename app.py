799bc5f5f82e10f8b448793bfd64eca9f7747f15
Manish Kumar <mkyadav2393@gmail.com>
2020-03-14 22:07:25 +0530

    changed app.run

--- app.py
+++ app.py
@@ -208,4 +208,5 @@ def go_back():
 
 if __name__ == '__main__':
     # app.run(debug=True)
-    app.run(debug=True, host = '0.0.0.0', port=5000)
+#     app.run(debug=True, host = '0.0.0.0', port=5000)
+    app.run(debug=True)
